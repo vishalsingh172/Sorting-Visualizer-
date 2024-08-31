29f6c8100f802f0cde56d525b33ce28b89d76e01
Vishal Kumar Singh <122435810+vishalsingh172@users.noreply.github.com>
2024-08-31 14:18:12 +0530

    Update insertion_sort.js

--- Sorting_Visualizer-master/scripts/insertion_sort.js
+++ Sorting_Visualizer-master/scripts/insertion_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
@@ -58,7 +58,7 @@ function Insertion()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
