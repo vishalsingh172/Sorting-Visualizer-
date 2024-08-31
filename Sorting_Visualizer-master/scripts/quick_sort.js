3cb79b71b1c0800258253057cdb6f319a496800a
Vishal Kumar Singh <122435810+vishalsingh172@users.noreply.github.com>
2024-08-31 14:19:59 +0530

    Update quick_sort.js

--- Sorting_Visualizer-master/scripts/quick_sort.js
+++ Sorting_Visualizer-master/scripts/quick_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
@@ -82,7 +82,7 @@ function quick_sort (start, end )
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
