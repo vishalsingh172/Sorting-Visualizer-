a43705c9b2dd1781419f4c3cd72d3d34b739cf6b
Vishal Kumar Singh <122435810+vishalsingh172@users.noreply.github.com>
2024-08-31 14:20:37 +0530

    Update selection_sort.js

--- Sorting_Visualizer-master/scripts/selection_sort.js
+++ Sorting_Visualizer-master/scripts/selection_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
@@ -61,7 +61,7 @@ function Selection_sort()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
