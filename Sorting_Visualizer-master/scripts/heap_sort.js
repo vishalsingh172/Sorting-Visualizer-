f68c3f30eb931b55ab4da64535e0e2b48800b721
Vishal Kumar Singh <122435810+vishalsingh172@users.noreply.github.com>
2024-08-31 14:17:39 +0530

    Update heap_sort.js

--- Sorting_Visualizer-master/scripts/heap_sort.js
+++ Sorting_Visualizer-master/scripts/heap_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
@@ -99,7 +99,7 @@ function heap_sort()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:- Vishal Kumar Singh
     
     *****************
 */
